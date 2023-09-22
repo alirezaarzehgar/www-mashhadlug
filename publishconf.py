5bb6f65112c005d687b1c7c77d623d060755d121
alireza <alirezaarzehgar82@gmail.com>
2023-09-22 05:36:27 +0330

    Update SITEURL on publishconf.py
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://mashhadlug.org'
+SITEURL = 'https://alirezaarzehgar.github.io/www-mashhadlug'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
